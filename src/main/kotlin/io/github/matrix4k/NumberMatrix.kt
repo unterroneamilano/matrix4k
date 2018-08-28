951efd129f7558bb8af7c0ff6d94cc24d2a996b5
Massimo Siani <massimo.siani@openmindonline.it>
2018-08-28 13:03:49 +0200

    Add the determinant

--- src/main/kotlin/io/github/matrix4k/NumberMatrix.kt
+++ src/main/kotlin/io/github/matrix4k/NumberMatrix.kt
@@ -31,6 +31,7 @@ operator fun <M: Number, N: Number> Matrix<M>.div(other: Matrix<N>): Matrix<Doub
 
 operator fun <M: Number> Matrix<M>.div(other: Number): Matrix<Double> = map { it.toDouble() / other.toDouble() }
 
+@Deprecated(message = "This function will be removed in a future version", replaceWith = ReplaceWith("dot"))
 infix fun <M: Number, N: Number> Matrix<M>.x(other: Matrix<N>): Matrix<Double> {
     if (rows != other.cols)
         throw IllegalArgumentException("The matrices do not match")
@@ -53,4 +54,29 @@ infix fun <M: Number, N: Number> Matrix<M>.dot(other: Matrix<N>): Matrix<Double>
             value += this[i, y].toDouble() * other[x, i].toDouble()
         value
     }
+}
+
+fun <M: Number> Matrix<M>.determinant(): Double = when {
+    rows != cols -> throw IllegalArgumentException("Cannot compute the determinant for non-squared matrices")
+    rows == 1 -> this[0, 0].toDouble()
+    rows == 2 -> this[0, 0].toDouble() * this[1, 1].toDouble() - (this[1, 0].toDouble() * this[0, 1].toDouble())
+    else -> {
+        var mvalue = 0.0
+        var negative = false
+        for (column in 0 until cols) {
+            val posValue = this[column, 0].toDouble() * determinantOfSubMatrix(0, column)
+            mvalue += if (negative) -posValue else posValue
+            negative = !negative
+        }
+        mvalue
+    }
+}
+
+private fun <M : Number> Matrix<M>.determinantOfSubMatrix(row: Int, col: Int): Double {
+    val list: MutableList<Double> = mutableListOf()
+    mapIndexed { x, y, value ->
+        if (x != col && y != row) list.add(value.toDouble())
+        value
+    }
+    return matrixOf(cols - 1, rows - 1, *list.toTypedArray()).determinant()
 }
